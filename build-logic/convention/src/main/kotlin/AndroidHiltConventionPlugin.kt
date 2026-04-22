b16d3f676fa85865fe963676d0f308f6ce4cce32
전형선 <hsun5163@gmail.com>
2026-04-22 23:53:33 +0900

    build(#5): Hilt 의존성 설정에 버전 카탈로그 적용
    - AndroidHiltConventionPlugin 내 하드코딩된 버전을 libs.versions.toml 참조로 변경

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -1,15 +1,19 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("com.google.devtools.ksp")
             pluginManager.apply("com.google.dagger.hilt.android")
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
-                add("implementation", "com.google.dagger:hilt-android:2.59.2")
-                add("ksp", "com.google.dagger:hilt-android-compiler:2.59.2")
+                add("implementation", libs.findLibrary("hilt-android").get())
+                add("ksp", libs.findLibrary("hilt-android-compiler").get())
             }
         }
     }
