02f573657ae0d5f27c733f99be222e38e9dc660c
전형선 <hsun5163@gmail.com>
2026-04-22 15:39:29 +0900

    init(#3): 안드로이드 프로젝트 생성
    - Jetpack Compose기반 프로젝트 생성
    - .gitignore 및 기본적인 폴더 구조 세팅
    
    close #3

---
+++ app/src/main/java/com/soma2026/lab/android/architecture/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.soma2026.lab.android.architecture
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.soma2026.lab.android.architecture.ui.theme.Soma2026labandroidarchitectureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Soma2026labandroidarchitectureTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Soma2026labandroidarchitectureTheme {
+        Greeting("Android")
+    }
+}
